b54bbeda04568be8a0c6bf6f4b0efb3c3a9e676e
muskaanahuja <90239794+muskaanahuja@users.noreply.github.com>
2021-09-20 23:56:48 +0400

    Update Muskaan Ahuja - Lab 2.2.py

--- .github/Muskaan Ahuja - Lab 2.2.py
+++ .github/Muskaan Ahuja - Lab 2.2.py
@@ -1,9 +1,11 @@
 import turtle
 def turtle_state():
-    turtle.isdown()
-    turtle.heading()
-    turtle.xcor(), turtle.ycor()
-def my_turtle():
+    isDown=turtle.isdown()
+    currAng=turtle.heading()
+    xCord=turtle.xcor()
+    yCord=turtle.ycor()
+    return isDown,currAng,xCord,yCord
+def test_drive():
     turtle.forward(100)
     turtle.left(87)
     turtle.setheading(127)
@@ -13,8 +15,19 @@ def my_turtle():
     turtle.home()
     turtle.circle(25)
 def main():
-    turtle_state()
-    my_turtle()
-    turtle_state()
+    stateBefore=turtle_state()
+    print("Before Drawing")
+    print("The Pen State is DOWN (T/F):"+str(stateBefore[0]))
+    print("The Angle of Orientation is:"+str(stateBefore[1]))
+    print("The X-Coordinate="+str(stateBefore[2])+" The Y-Coordinate="+str(stateBefore[3]))
+
+    test_drive()
+
+    stateAfter=turtle_state()
+    print("After Drawing")
+    print("The Pen State is DOWN (T/F):"+str(stateAfter[0]))
+    print("The Angle of Orientation is:"+str(stateAfter[1]))
+    print("The X-Coordinate="+str(stateAfter[2])+" The Y-Coordinate="+str(stateAfter[3]))
+
     input("press any key to continue..")
 main()
