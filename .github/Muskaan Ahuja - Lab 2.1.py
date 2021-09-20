0df69646ced33b896e00008e079eb3c6f460017a
muskaanahuja <90239794+muskaanahuja@users.noreply.github.com>
2021-09-20 22:30:47 +0400

    Add files via upload

---
+++ .github/Muskaan Ahuja - Lab 2.1.py
@@ -0,0 +1,14 @@
+import turtle
+def my_turtle():
+    turtle.forward(100)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.up()
+    turtle.goto(50,40)
+    turtle.down()
+    turtle.home()
+    turtle.circle(25)
+def main():
+    my_turtle()
+    input("press any key to continue..")
+main()
